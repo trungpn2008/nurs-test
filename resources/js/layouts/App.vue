68667a9f1edef2334521076644f6070a676238ec
Duong Thanh Tung <tungdt1@dxmb.vn>
2022-07-06 10:42:30 +0700

    aa

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="warpper">
+        <router-view name="header"></router-view>
+        <router-view></router-view>
+        <router-view name="footer"></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/routers.js
+++ resources/js/routers.js
@@ -1,20 +1,12 @@
-import home from './components/home/home'
-import home2 from './components/home/home2'
-import frontend from "./layouts/frontend";
 const routers = [
     {
-        path: '/home',
-        component: home,
-        name: 'pc-top1',
-    }, {
         path: '/',
-        component: frontend,
+        components: {
+            header:()=> import('./layouts/header.vue'),
+            footer:()=> import('./layouts/footer/footer.vue'),
+        },
         name: 'pc-top1',
     },
-    {
-        path: '/home2',
-        component: home2,
-        name: 'pc-top2',
-    }
+
 ];
 export default routers;
