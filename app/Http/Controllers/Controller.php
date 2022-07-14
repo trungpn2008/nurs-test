cbfe161ba9aab4f93d762bbf790710a97bb21665
Pham Ngoc Trung PCN <trungpn@dxmb.com>
2022-07-14 12:34:40 +0700

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Data\Operator;
+use App\Models\BaseModel;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Modules\Admin\Entities\UserRolePermission;
 
 class Controller extends BaseController
 {
@@ -99,5 +103,165 @@ class Controller extends BaseController
     }
     protected static function jsonError($reason = "Không thành công!", $data = [], $logs = []) {
         return response()->json(['success' => false, 'msg' => $reason, 'data' => $data], 200);
+    }
+    public static function stripVN($str) {
+        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
+        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
+        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
+        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
+        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
+        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+        $str = preg_replace("/(đ)/", 'd', $str);
+
+        $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
+        $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
+        $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
+        $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
+        $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
+        $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+        $str = preg_replace("/(Đ)/", 'D', $str);
+        return $str;
+    }
+    public static function convert_number_to_words($number) {
+        $hyphen      = ' ';
+        $conjunction = '  ';
+        $separator   = ' ';
+        $negative    = 'âm ';
+        $decimal     = ' phẩy ';
+        $dictionary  = array(
+            0                   => 'không',
+            1                   => 'một',
+            2                   => 'hai',
+            3                   => 'ba',
+            4                   => 'bốn',
+            5                   => 'năm',
+            6                   => 'sáu',
+            7                   => 'bảy',
+            8                   => 'tám',
+            9                   => 'chín',
+            10                  => 'mười',
+            11                  => 'mười một',
+            12                  => 'mười hai',
+            13                  => 'mười ba',
+            14                  => 'mười bốn',
+            15                  => 'mười năm',
+            16                  => 'mười sáu',
+            17                  => 'mười bảy',
+            18                  => 'mười tám',
+            19                  => 'mười chín',
+            20                  => 'hai mươi',
+            30                  => 'ba mươi',
+            40                  => 'bốn mươi',
+            50                  => 'năm mươi',
+            60                  => 'sáu mươi',
+            70                  => 'bảy mươi',
+            80                  => 'tám mươi',
+            90                  => 'chín mươi',
+            100                 => 'trăm',
+            1000                => 'nghìn',
+            1000000             => 'triệu',
+            1000000000          => 'tỷ',
+            1000000000000       => 'nghìn tỷ',
+            1000000000000000    => 'nghìn triệu triệu',
+            1000000000000000000 => 'tỷ tỷ'
+        );
+        if (!is_numeric($number)) {
+            return false;
+        }
+        if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
+            // overflow
+            trigger_error(
+                'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX,
+                E_USER_WARNING
+            );
+            return false;
+        }
+        if ($number < 0) {
+            return $negative . self::convert_number_to_words(abs($number));
+        }
+        $string = $fraction = null;
+        if (strpos($number, '.') !== false) {
+            list($number, $fraction) = explode('.', $number);
+        }
+        switch (true) {
+            case $number < 21:
+                $string = $dictionary[$number];
+                break;
+            case $number < 100:
+                $tens   = ((int) ($number / 10)) * 10;
+                $units  = $number % 10;
+                $string = $dictionary[$tens];
+                if ($units) {
+                    $string .= $hyphen . $dictionary[$units];
+                }
+                break;
+            case $number < 1000:
+                $hundreds  = $number / 100;
+                $remainder = $number % 100;
+                $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+                if ($remainder) {
+                    $string .= $conjunction . self::convert_number_to_words($remainder);
+                }
+                break;
+            default:
+                $baseUnit = pow(1000, floor(log($number, 1000)));
+                $numBaseUnits = (int) ($number / $baseUnit);
+                $remainder = $number % $baseUnit;
+                $string = self::convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+                if ($remainder) {
+                    $string .= $remainder < 100 ? $conjunction : $separator;
+                    $string .= self::convert_number_to_words($remainder);
+                }
+                break;
+        }
+        if (null !== $fraction && is_numeric($fraction)) {
+            $string .= $decimal;
+            $words = array();
+            foreach (str_split((string) $fraction) as $number) {
+                $words[] = $dictionary[$number];
+            }
+            $string .= implode(' ', $words);
+        }
+        return $string;
+    }
+
+    /**
+     * @param int $length
+     * @return string
+     */
+    protected static function randString($length = 6) {
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    public function authorize()
+    {
+        if(Auth::user()){
+            $check_role = (new UserRolePermission())->select(['user_role_permission.user_id','user_role_permission.role_id','role_permission.permission','user_role_permission.permission as user_permission'])->join(new Operator(null,null,'role_permission','user_role_permission.role_id','role_permission.role_id'))->whereOperator([new Operator('user_role_permission.user_id',Auth::user()->id),new Operator('user_role_permission.status',1),new Operator('user_role_permission.deleted_at',null)])->builder();
+            if($check_role){
+                $role = $check_role->role_id;
+                $permissions = $this->megeRole($check_role->permission,$check_role->user_permission);
+            }else{
+                $role = null;
+                $permissions = null;
+            }
+            return ['rol'=>$role,'super'=>Auth::user()->is_super_admin,'perms'=>$permissions];
+        }
+    }
+    public function megeRole($permission = [],$user_permission = []){
+        $permission = json_decode($permission,true);
+        $userPermission = json_decode($user_permission,true)??[];
+        foreach ($permission as $key =>$item){
+            if(isset($userPermission[$key])){
+                $permissions[$key] = array_merge(array_diff($item,$userPermission[$key]),array_diff($userPermission[$key],$item),array_intersect($item,$userPermission[$key]));
+            }else{
+                $permissions[$key] = $item;
+            }
+        }
+        return array_merge($permissions,array_diff_key($userPermission,$permission));
     }
 }
