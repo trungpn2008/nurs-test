2d75330ba98207e3b61c79b6e3a776037581ad25
Pham Ngoc Trung PCN <trungpn@dxmb.com>
2022-07-27 17:41:04 +0700

    upodate

--- resources/js/pages/advice/index.vue
+++ resources/js/pages/advice/index.vue
@@ -116,158 +116,11 @@
                     <div class="advice break">
                         <div class="box">
                             <p class="bg-title">
-							<span>
-								みんなの法律相談ガイドライン
-							</span>
+							<span>{{ cate.title }}</span>
                             </p>
-                            <div class="info">
-                                <p class="title">
-                                    <span>はじめに</span>
-                                </p>
-                                <div class="text-box">
-                                    「弁護士ドットコム」は、「弁護士をもっと身近に」という理念に基づき、弁護士と依頼者とをつなぐプラットフォームを提供することを主な目的としています。<br/>
-                                    「みんなの法律相談」は、当サイトの登録弁護士が、実名で一般利用者のお悩みを解決する公開型Q&Aサービスです。<br/>
-                                    投稿された多くの相談は、法的問題の解決データとして、投稿した一般利用者だけでなく、同様のお悩みを抱える閲覧した一般利用者にもお役立ていただくため、公開型としております。<br/>
-                                    「みんなの法律相談」において、このようなサービスの目的を達成するために、ご利用にあたって遵守いただきたい事項を、「みんなの法律相談　ガイドライン」として規定しました。<br/>
-                                    一般利用者の皆様には、「みんなの法律相談」のこのような目的をご理解いただき、ご賛同いただいたうえで本ガイドラインに沿ったご利用をお願いします。<br/>
-                                    本ガイドラインに違反する書き込みについては、下記に個別に規定した場合を含め、当社の判断により修正や削除をさせていただく場合がございます。
-                                    あらかじめご了承ください。
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第１ 実際にお悩みになっていることをご投稿ください。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        質問を投稿する際は、ご自身が抱えているお悩みについて、回答する登録弁護士にわかりやすい内容で投稿するようお願いします。<br/>
-                                        架空の相談等、本サービスの目的や趣旨に則っていないご相談はお控えください。<br/>
-                                        例えば、次のような投稿は、これに該当します。
-                                    </p>
-                                    <ol>
-                                        <li>大学のレポート課題等、学術目的のための内容
-                                        </li>
-                                        <li>特定の企業または個人の評判を落とすことを目的とした架空の内容
-                                        </li>
-                                        <li>ニュースや外部記事などに対して、見解を知りたいだけのもの
-
-                                        </li>
-                                        <li>お悩みと直接関係ない文章が大半を占めるようなもの</li>
-                                        <li>過度な伏字や隠語の使用により文意が伝わりにくいもの</li>
-                                        <li>複数行にわたるアスキーアートにより、環境によって正しく表示されない可能性があるもの</li>
-
-                                        <li>弁護士と直接連絡をとる目的で登録弁護士からの連絡を促すもの</li>
-
-                                    </ol>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第２ 回答した登録弁護士と「みんなの法律相談」上で、議論を行うことはご遠慮ください。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        回答した登録弁護士の回答で疑問が解消しなかった場合、端的にわからない点を挙げ追加の質問をするようにお願いします。<br/>
-                                        必要以上に回答した登録弁護士に対し喧嘩腰で議論を挑む、非難をする、誹謗中傷するような行為は厳に慎むようお願いします。
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第３ 質問者の方やご相談内容に出てくる関係者の方など、個人や組織が特定されないようにご投稿ください。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        「みんなの法律相談」は公開型のQ&Aサービスです。インターネットへ接続できる環境のある方なら、どなたでも見ることができますので、投稿の際は、ご自身や関係者の個人や組織が特定されうる内容の記載にご注意ください。<br/>
-                                        当サイトでは、原則として投稿削除のご依頼は受け付けておりません。<br/>
-                                        個人や組織が特定されうる内容としては、次のような投稿が該当します。
-                                    </p>
-                                    <ol>
-                                        <li>場所についての詳細を記載しているもの（例：番地や建物名、部屋番号など）</li>
-                                        <li>人物についての詳細を記載しているもの（例：氏名や勤務先、所属先の組織名、電話番号、メールアドレスなど）</li>
-                                        <li>年月日や時系列の詳細を記載しているもの</li>
-                                        <li>上記の組み合わせによって個人や組織が特定されうるもの</li>
-                                    </ol>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第４ 誹謗中傷を含む投稿や公序良俗に反する投稿は禁止します。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        回答した登録弁護士を含む他者（企業やサービス、商品なども含む）を誹謗中傷する投稿、喧嘩腰で議論を挑む行為、侮辱的ないし不快な投稿や、卑猥な表現等、公序良俗に反する投稿は厳に慎むようお願いします。<br/>
-                                        例えば、次のような内容は、これに該当します。
-                                    </p>
-                                    <ol>
-                                        <li>相手への過剰な批判（例：「弁護士のくせに・・・」「・・・なんて信じられない。」）</li>
-                                        <li>差別的な内容（例：「◯◯（人種等）なので」「◯◯（人格を否定する用語）のくせに」）</li>
-                                        <li>特定の企業または個人の評判を落とす、不信感を抱かせるなどのおそれがある内容</li>
-                                        <li>明記をせずとも内容が伝わるにもかかわらず、体の部位や性的行為を明記するなど、卑猥な内容が含まれる投稿</li>
-                                    </ol>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第５ URLを使用した投稿はお控えください。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        ウイルスに感染するサイトや、詐欺等を目的とする悪質なサイトへ誘導するなど、不要なトラブルが発生する可能性があるため、URLの使用はお控えください。<br/>
-                                        なお、相談内容への補足情報として、当社が許可するURLはご使用いただくことができます。<br/>
-                                        使用を許可していないURLと判断された場合には、URL部分の削除を行います。<br/>
-                                        そのため、原則として文章だけで内容が伝わるようご記載ください。<br/>
-                                        使用を許可するURLは次に該当するものです。
-                                    </p>
-                                    <ol>
-                                        <li>当社運営サービス「弁護士ドットコム」（bengo4.com）、「税理士ドットコム」（zeiri4.com）、「クラウドサイン」（cloudsign.jp）等
-                                        </li>
-                                        <li>裁判所のウェブサイト等の国営サイト（go.jpドメイン）</li>
-                                        <li>学術機関のウェブサイト（ac.jpドメイン）</li>
-                                        <li>都道府県や市区町村の地方公共団体のサイト</li>
-                                        <li>官公庁や警察庁、検察庁などの行政機関のサイト</li>
-                                    </ol>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第６ 利用規約に違反するような投稿は禁止します。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        「みんなの法律相談」は、「弁護士ドットコム」のサービスの一部ですので、利用規約等のルールに則った投稿をお願いします。<br/>
-                                        利用規約・プライバシーの考え方（https://www.bengo4.com/sp/static/rules/）
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>第７ 原本の保持をお願いします。</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        「みんなの法律相談」へ投稿された質問や回答の内容が本ガイドラインに違反する場合、投稿した一般利用者や登録弁護士への予告なく削除、修正をする場合がありますので、投稿に関する文章の原本はご自身で保持するようお願いします。
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <p class="title">
-                                    <span>附則</span>
-                                </p>
-                                <div class="text-box">
-                                    <p class="text">
-                                        （施行期日等）<br/>
-                                        2017年3月1日 制定・施行<br/>
-                                        2019年7月1日 改定<br/>
-                                        2021年9月28日 改定<br/>
-                                        （実名のみの回答に関する経過措置）
-
-                                    </p>
-                                    <p class="text">
-                                        「みんなの法律相談」における当サイトの登録弁護士による回答についてすべて実名による回答となるのは、2021年9月28日改訂以降の回答についてとなります。
-
-                                    </p>
-                                </div>
+                            <div class="info"  v-for="(item, index) in content">
+                                <p class="title"><span>{{ item.title }}</span></p>
+                                <div class="text-box" v-html="item.content"></div>
                             </div>
                         </div>
                     </div>
@@ -306,7 +159,46 @@
 <script>
 import Infor from '../../components/Infor.vue';
 export default {
-    components: { Infor }
+    components: { Infor },
+    data() {
+        return {
+            cate: {
+                title:null,
+                parent_id:null,
+            },
+            content: [],
+        };
+    },
+    methods: {
+        async getadvice() {
+            let { data } = await this.axios.get("api/term-and-condition-category/detail/1", {
+                params: { type: 1 }
+                // auth: {
+                //     username: "care21@greentechsolutions",
+                //     password: "care21greentech@"
+                // },
+            });
+            this.cate = data.data.category;
+            this.content = data.data.content;
+        },
+    },
+    computed: {
+        imageUrl() {
+            return this.banner2.image;
+        },
+        imageicon3() {
+            return this.banner3.icon;
+        },
+        imageicon4() {
+            return this.banner4.icon;
+        },
+        imageicon5() {
+            return this.banner5.icon;
+        }
+    },
+    async mounted() {
+        await this.getadvice();
+    },
 }
 </script>
 
